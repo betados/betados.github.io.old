a89569700701b04ab1d149b647a2f6dee51f03d3
Alvaro <torrestal@gmail.com>
2018-02-10 18:58:35 +0100

    added button

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function over(esto){
+    document.getElementById(esto.alt).innerHTML= "<font color=#FFFFFF><b> Súbete al tren 🚂 para conocer más sobre mí </b></font>"
+}
+
+function out(esto){
+    document.getElementById(esto.alt).innerHTML = ""
+}
+
+
